b62dfcbe7288f282e0a576a6fc9d4f24dcdfad5e
blaise-faurie <blaisefaurie@gmail.com>
2021-07-21 17:05:59 +0200

    jour 3

--- wine_spirit.py
+++ wine_spirit.py
@@ -5,6 +5,24 @@ Created on Tue Jul 20 10:12:04 2021
 @author: Utilisateur
 """
 
-'''
-test wine_spirit
-'''
+import pandas as pd
+import numpy as np
+
+# tansform the xlsx file to dataframe
+df_web = pd.read_excel("web.xlsx")
+df_erp = pd.read_excel("erp.xlsx")
+df_liaison = pd.read_excel("liaison.xlsx")
+
+df_web_origin, df_erp_origin, df_liaison_origin = df_web, df_erp, df_liaison
+
+print(df_web.head())
+print(df_web.info())
+print(df_web.describe())
+
+
+df_1 = df_web.merge(df_liaison, left_on='sku', right_on='id_web')
+
+df_1_origin = df_1
+
+df_1.drop_duplicates()
+
